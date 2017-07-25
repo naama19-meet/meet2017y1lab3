43788752f0ad60fe0b5fe223c357c3ca04eb853b
naama19-meet <naama19@meet.mit.edu>
2017-07-25 12:23:18 +0300

    Stopping here

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print('Γειά σου Κόσμε is how you say hello world in greek')
+print('Olá Mundo is how you say hello world in portuguese')
+print('Ahoj světe is how you say hello world in czach') 
+print('안녕 세상 is how you say hello world in korean')
+print('你好，世界 is how you say hello world in chinese')
+print('नमस्ते दुनिया is how you say hello world in hindi')
+print('Salamu, Dunia is how you say hello world in swahili')
+print('שלום עולם is how you say hello world in hebrew')
