1845ddcddc089b2b462ef3567da1bb46b4f297a5
naama19-meet <naama19@meet.mit.edu>
2017-07-25 18:35:05 +0300

    Stopping here

---
+++ Ages.py
@@ -0,0 +1,6 @@
+print('what is your age?')
+age = input
+print('what is your age?')
+age = input
+print(age + age +'sum')
+print('sum')
---
+++ ages.py
@@ -0,0 +1,110 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/inputoutput.py =====
+How old are you?
+15
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/inputoutput.py =====
+How old are you?
+15
+Wow, you're 15 years old!
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/inputoutput.py =====
+How old are you?
+15
+Wow, you're 15 years old!
+How old are you? 15
+Wow, you're 15 years old!
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/repeatsongs.pt =====
+Row your boat
+Gently down the stream.
+Merrily
+Life is but a dream.
+
+
+And I was like
+Baby
+oooooooh!
+Baby
+nooooooo!
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/repeatsongs.pt =====
+Row Row Row  your boat
+Gently down the stream.
+Merrily Merrily Merrily 
+Life is but a dream.
+
+
+And I was like
+Baby
+oooooooh!
+Baby
+nooooooo!
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 1, in <module>
+    age_a = imput('what is your age?')
+NameError: name 'imput' is not defined
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 2, in <module>
+    age = imput
+NameError: name 'imput' is not defined
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 2, in <module>
+    age1 = imput
+NameError: name 'imput' is not defined
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+what is your age?
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 2, in <module>
+    age1 = imput
+NameError: name 'imput' is not defined
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+what is your age?
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 2, in <module>
+    age = imput
+NameError: name 'imput' is not defined
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+what is your age?
+what is your age?
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 5, in <module>
+    print(age_a + age_b +'sum')
+NameError: name 'age_a' is not defined
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+what is your age?
+what is your age?
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 5, in <module>
+    print(age_a + age_b +'sum')
+NameError: name 'age_a' is not defined
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+what is your age?
+what is your age?
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 5, in <module>
+    print(age1 + age2 +'sum')
+TypeError: unsupported operand type(s) for +: 'builtin_function_or_method' and 'builtin_function_or_method'
+>>> 
+========= RESTART: /home/student/naama19_lab3/meet2017y1lab3/Ages.py =========
+what is your age?
+what is your age?
+Traceback (most recent call last):
+  File "/home/student/naama19_lab3/meet2017y1lab3/Ages.py", line 5, in <module>
+    print(age + age +'sum')
+TypeError: unsupported operand type(s) for +: 'builtin_function_or_method' and 'builtin_function_or_method'
+>>> 
