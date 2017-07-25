0d5b9de23298c12cbfcede2dfd89ed05c77885dc
naama19-meet <naama19@meet.mit.edu>
2017-07-25 12:25:28 +0300

    Stopping here

---
+++ work.py
@@ -0,0 +1,84 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print('hello world')
+hello world
+>>> #print(hello world')
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+Olá Mundo is hoe you say hello world in portuguese
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+Olá Mundo is how you say hello world in portuguese
+Ahoj světe is how you say hello world in czach
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+Olá Mundo is how you say hello world in portuguese
+Ahoj světe is how you say hello world in czach
+안녕 세상 is how you say hello world in korean
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+Olá Mundo is how you say hello world in portuguese
+Ahoj světe is how you say hello world in czach
+안녕 세상 is how you say hello world in korean
+你好，世界 is how you say hello world in chinese
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+Olá Mundo is how you say hello world in portuguese
+Ahoj světe is how you say hello world in czach
+안녕 세상 is how you say hello world in korean
+你好，世界 is how you say hello world in chinese
+नमस्ते दुनिया is how you say hello world in hindi
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+Olá Mundo is how you say hello world in portuguese
+Ahoj světe is how you say hello world in czach
+안녕 세상 is how you say hello world in korean
+你好，世界 is how you say hello world in chinese
+नमस्ते दुनिया is how you say hello world in hindi
+Salamu, Dunia is how you say hello world in swahili
+>>> 
+====== RESTART: /home/student/naama19_lab3/meet2017y1lab3/helloworld.py ======
+Γειά σου Κόσμε is how you say hello world in greek
+Olá Mundo is how you say hello world in portuguese
+Ahoj světe is how you say hello world in czach
+안녕 세상 is how you say hello world in korean
+你好，世界 is how you say hello world in chinese
+नमस्ते दुनिया is how you say hello world in hindi
+Salamu, Dunia is how you say hello world in swahili
+שלום עולם is how you say hello world in hebrew
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/robertfrost.py =====
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/robertfrost.py =====
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/robertfrost.py =====
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/robertfrost.py =====
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
+===== RESTART: /home/student/naama19_lab3/meet2017y1lab3/robertfrost.py =====
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
